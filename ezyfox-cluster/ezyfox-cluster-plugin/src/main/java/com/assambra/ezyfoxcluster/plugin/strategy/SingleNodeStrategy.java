18902ed25fc32e35eb911ced044fe71910d79a15
Assambra <philipp.leutner@assambra.com>
2024-10-17 13:29:54 +0200

    SingleNodeStrategy use now both roles leader and worker

--- ezyfox-cluster/ezyfox-cluster-plugin/src/main/java/com/assambra/ezyfoxcluster/plugin/strategy/SingleNodeStrategy.java
+++ ezyfox-cluster/ezyfox-cluster-plugin/src/main/java/com/assambra/ezyfoxcluster/plugin/strategy/SingleNodeStrategy.java
@@ -4,8 +4,19 @@ import com.tvd12.ezyfox.util.EzyLoggable;
 import com.tvd12.ezyfoxserver.context.EzyPluginContext;
 
 public class SingleNodeStrategy extends EzyLoggable implements NodeStrategy {
+
+    private final LeaderNodeStrategy leaderNodeStrategy;
+    private final WorkerNodeStrategy workerNodeStrategy;
+
+    public SingleNodeStrategy() {
+        this.leaderNodeStrategy = new LeaderNodeStrategy();
+        this.workerNodeStrategy = new WorkerNodeStrategy();
+    }
+
     @Override
     public void initialize(EzyPluginContext context) {
         logger.info("Initializing Single Node");
+        leaderNodeStrategy.initialize(context);
+        workerNodeStrategy.initialize(context);
     }
 }
