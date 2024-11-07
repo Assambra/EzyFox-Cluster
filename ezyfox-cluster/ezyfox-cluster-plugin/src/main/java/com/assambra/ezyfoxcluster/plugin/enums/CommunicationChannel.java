3e3837e49c08a8236924f04594b4ec4193b3ea74
Assambra <philipp.leutner@assambra.com>
2024-11-07 21:47:49 +0100

    We use now a string in the enum that match with namespace

--- ezyfox-cluster/ezyfox-cluster-plugin/src/main/java/com/assambra/ezyfoxcluster/plugin/enums/CommunicationChannel.java
+++ ezyfox-cluster/ezyfox-cluster-plugin/src/main/java/com/assambra/ezyfoxcluster/plugin/enums/CommunicationChannel.java
@@ -1,9 +1,18 @@
 package com.assambra.ezyfoxcluster.plugin.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum CommunicationChannel {
-    GUEST_COM,
-    PLAYER_COM,
-    SERVER_COM,
-    ROOM_COM,
-    CONTROL_COM
+    PLAYER_COM("player_com"),
+    GUEST_COM("guest_com"),
+    SERVER_COM("server_com"),
+    ROOM_COM("room_com"),
+    CONTROL_COM("control_com");
+
+    private final String channelName;
+
+    CommunicationChannel(String channelName) {
+        this.channelName = channelName;
+    }
 }
